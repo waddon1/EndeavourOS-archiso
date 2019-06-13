f0650d4417b572bbf826561265bcde10a5daaa98
portergos <portergoslinux@gmail.com>
2019-06-13 17:40:56 -0300

    fix

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -9,7 +9,10 @@ chown root:root $(find ./airootfs |grep -v "/etc/skel")
 chmod 700 ./airootfs/root
 chmod -R 755 airootfs/etc/sudoers.d
 chmod 755 ./airootfs/etc/sudoers.d
-chmod 440 ./airootfs/etc/sudoers.d/g_wheel
+chmod 755 ./airootfs/root
+
+# The iso creation will handle the file later, otherwise uploading to git gets complicated
+chmod 444 ./airootfs/etc/sudoers.d/g_wheel
 #chmod 644 ./airootfs/etc/systemd/system/*.service
 chown 0 ./airootfs/etc/sudoers.d
 chown 0 ./airootfs/etc/sudoers.d/g_wheel
@@ -18,7 +21,8 @@ chown root:root ./airootfs/etc/sudoers.d/g_wheel
 chmod 755 ./airootfs/etc
 
 chmod +x ./airootfs/offline_installer/installer
-chmod +x ./airootfs/etc/skel/Desktop/{offline.desktop,online.desktop}
+#chmod +x ./airootfs/etc/skel/Desktop/{offline.desktop}
+#,online.desktop}
 #,calamares.desktop}
 chmod +x ./airootfs/usr/bin/cleaner_script.sh
 
