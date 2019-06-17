0a345d7b45a74f96dffdb1fd46085974725cb571
portergos <portergoslinux@gmail.com>
2019-06-16 23:59:26 -0300

    minor updates, calamares fail

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -11,6 +11,26 @@ arch_chroot(){
     arch-chroot $script_path/${work_dir}/airootfs /bin/bash -c "${1}"
 }  
 
+do_merge(){
+
+arch_chroot "pacman-key --init
+pacman-key --add /usr/share/pacman/keyrings/endeavour.gpg && sudo pacman-key --lsign-key 497AF50C92AD2384C56E1ACA003DB8B0CB23504F
+pacman-key --populate
+pacman-key --refresh-keys
+pacman -Syy
+export DISPLAY=:0.0
+dbus-launch dconf load / < /etc/skel/dconf.conf
+sudo -H -u liveuser bash -c 'dbus-launch dconf load / < /etc/skel/dconf.conf'
+rm /etc/skel/dconf.conf
+rm /home/liveuser/dconf.conf
+sed -i 's~\#GRUB_BACKGROUND=.*~GRUB_BACKGROUND=\/usr\/share\/endeavour\/endeavouros-wallpaper.png~g' /etc/default/grub
+echo 'GRUB_DISABLE_SUBMENU=y' >> /etc/default/grub
+pacman -U /etc/skel/calamares_installer-1-1-x86_64.pkg.tar.xz --noconfirm
+rm /etc/skel/calamares_installer-1-1-x86_64.pkg.tar.xz
+rm /home/liveuser/calamares_installer-1-1-x86_64.pkg.tar.xz"
+
+}
+
 do_pacman_fix(){
 
 arch_chroot "pacman-key --init
@@ -48,7 +68,8 @@ rm /home/liveuser/calamares_installer-1-1-x86_64.pkg.tar.xz"
 ########## STARTS HERE ##########
 #################################
 
-do_pacman_fix
-do_dconf_dump
-do_grub_config
-do_install_extra
+do_merge
+#do_pacman_fix
+#do_dconf_dump
+#do_grub_config
+#do_install_extra
