3578eadcab4d4ccad4d7e7e292296294a9604c5f
portergos <portergoslinux@gmail.com>
2019-07-02 19:54:51 -0300

    symlink wallpaper, remove calamares hack

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -18,18 +18,18 @@ pacman-key --add /usr/share/pacman/keyrings/endeavour.gpg && sudo pacman-key --l
 pacman-key --populate
 pacman-key --refresh-keys
 pacman -Syy
-export DISPLAY=:0.0
+#export DISPLAY=:0.0
 #dbus-launch dconf load / < /etc/skel/dconf.conf
 #sudo -H -u liveuser bash -c 'dbus-launch dconf load / < /etc/skel/dconf.conf'
 #rm /etc/skel/dconf.conf
 #rm /home/liveuser/dconf.conf
 sed -i 's~\#GRUB_BACKGROUND=.*~GRUB_BACKGROUND=\/usr\/share\/endeavouros\/splash.png~g' /etc/default/grub
 echo 'GRUB_DISABLE_SUBMENU=y' >> /etc/default/grub
-pacman -U /etc/skel/calamares_installer-1-1-x86_64.pkg.tar.xz --noconfirm
-rm /etc/skel/calamares_installer-1-1-x86_64.pkg.tar.xz
-rm /home/liveuser/calamares_installer-1-1-x86_64.pkg.tar.xz
-rm /root/calamares_installer-1-1-x86_64.pkg.tar.xz
-rm /root/install.txt
+#pacman -U /etc/skel/calamares_installer-1-1-x86_64.pkg.tar.xz --noconfirm
+#rm /etc/skel/calamares_installer-1-1-x86_64.pkg.tar.xz
+#rm /home/liveuser/calamares_installer-1-1-x86_64.pkg.tar.xz
+#rm /root/calamares_installer-1-1-x86_64.pkg.tar.xz
+#rm /root/install.txt
 wget https://github.com/Portergos/calamares/raw/master/cleaner_script.sh
 chmod +x cleaner_script.sh
 mv cleaner_script.sh /usr/bin/cleaner_script.sh"
