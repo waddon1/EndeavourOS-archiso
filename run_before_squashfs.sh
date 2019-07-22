c28ff875c99af1f6241b5e538a85c040f80d5c1d
Portergos Linux <portergoslinux@gmail.com>
2019-07-22 12:10:24 -0300

    Update run_before_squashfs.sh

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -21,7 +21,7 @@ pacman -Syy
 #sed -i 's~\#GRUB_BACKGROUND=.*~GRUB_BACKGROUND=\/usr\/share\/endeavouros\/splash.png~g' /etc/default/grub
 sed -i 's~\#GRUB_THEME=.*~GRUB_THEME=\/boot\/grub\/themes\/EndeavourOS\/theme.txt~g' /etc/default/grub
 echo 'GRUB_DISABLE_SUBMENU=y' >> /etc/default/grub
-wget https://github.com/Portergos/calamares/raw/master/cleaner_script.sh
+wget https://github.com/endeavouros-team/install-scripts/raw/master/cleaner_script.sh
 chmod +x cleaner_script.sh
 mv cleaner_script.sh /usr/bin/cleaner_script.sh"
 
