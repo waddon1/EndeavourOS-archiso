49f9c91ee21439ad39b5b973f0f626a76de8edc2
Portergos Linux <portergoslinux@gmail.com>
2019-09-23 15:59:38 -0300

    Update run_before_squashfs.sh

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -24,9 +24,10 @@ echo 'GRUB_DISABLE_SUBMENU=y' >> /etc/default/grub
 wget https://github.com/endeavouros-team/install-scripts/raw/master/cleaner_script.sh
 wget https://github.com/endeavouros-team/install-scripts/raw/master/calamares_switcher
 wget https://github.com/endeavouros-team/install-scripts/raw/master/pacstrap_calamares
+wget https://github.com/endeavouros-team/install-scripts/raw/master/pacstrap_endeavouros
 wget https://raw.githubusercontent.com/endeavouros-team/install-scripts/master/update-mirrorlist
-chmod +x cleaner_script.sh calamares_switcher pacstrap_calamares update-mirrorlist
-mv cleaner_script.sh calamares_switcher update-mirrorlist pacstrap_calamares /usr/bin/
+chmod +x cleaner_script.sh calamares_switcher pacstrap_calamares pacstrap_endeavouros update-mirrorlist
+mv cleaner_script.sh calamares_switcher update-mirrorlist pacstrap_calamares pacstrap_endeavouros /usr/bin/
 wget https://raw.githubusercontent.com/endeavouros-team/liveuser-desktop-settings/master/dconf/mousepad.dconf
 dbus-launch dconf load / < mousepad.dconf
 sudo -H -u liveuser bash -c 'dbus-launch dconf load / < mousepad.dconf'
