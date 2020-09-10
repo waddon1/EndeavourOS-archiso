09aeb613b242ea4491654e48facc6e773ea436df
Portergos Linux <portergoslinux@gmail.com>
2020-09-09 21:02:03 -0300

    Update run_before_squashfs.sh

--- run_before_squashfs.sh
+++ run_before_squashfs.sh
@@ -28,9 +28,8 @@ wget https://github.com/endeavouros-team/install-scripts/raw/master/calamares_sw
 wget https://github.com/endeavouros-team/install-scripts/raw/master/pacstrap_calamares
 wget https://raw.githubusercontent.com/endeavouros-team/install-scripts/master/update-mirrorlist
 wget https://github.com/endeavouros-team/install-scripts/raw/master/calamares_for_testers
-wget https://github.com/endeavouros-team/install-scripts/raw/master/rank_pacman_key.sh
-chmod +x cleaner_script.sh chrooted_cleaner_script.sh calamares_switcher pacstrap_calamares update-mirrorlist calamares_for_testers rank_pacman_key.sh
-mv cleaner_script.sh chrooted_cleaner_script.sh calamares_switcher update-mirrorlist pacstrap_calamares calamares_for_testers rank_pacman_key.sh /usr/bin/
+chmod +x cleaner_script.sh chrooted_cleaner_script.sh calamares_switcher pacstrap_calamares update-mirrorlist calamares_for_testers
+mv cleaner_script.sh chrooted_cleaner_script.sh calamares_switcher update-mirrorlist pacstrap_calamares calamares_for_testers /usr/bin/
 wget https://raw.githubusercontent.com/endeavouros-team/liveuser-desktop-settings/master/dconf/mousepad.dconf
 dbus-launch dconf load / < mousepad.dconf
 sudo -H -u liveuser bash -c 'dbus-launch dconf load / < mousepad.dconf'
